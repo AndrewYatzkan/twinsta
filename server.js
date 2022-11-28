4a214b4d61a54a59850b7d3988d642fbe8b3e61f
AndrewYatzkan <andrew@yatzkan.com>
2022-11-27 21:00:55 -0600

    Add CORS

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 8080;
 const { runCompletion } = require('./gpt3');
 const { getUserId, getUserTweets } = require('./twitter');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
